257ead7f219f51df5908c6d16c6a0e5f47f2bae0
Animesh Agrawal <animesh.agrawal21@gmail.com>
2020-07-07 22:18:05 +0530

    Initial commits

--- src/main/java/com/simple/service/starter/Application.java
+++ src/main/java/com/simple/service/starter/Application.java
@@ -1,9 +1,12 @@
-package com.service.starter;
+package com.simple.service.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.simple.service")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
